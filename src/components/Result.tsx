9637761531f530bd733f79df8bac1186adfb6b94
Aditya Phasu <adityaphasu919@gmail.com>
2024-05-19 23:42:34 +0530

    Update Result component to dynamically handle tip and total amounts

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,19 +1,38 @@
-const Result = () => {
+import { twMerge } from "tailwind-merge";
+
+type ResultProps = {
+  tipAmount: number;
+  totalAmount: number;
+};
+
+const Result = ({ tipAmount, totalAmount }: ResultProps) => {
   return (
     <div className="space-y-6 lg:space-y-[3.25rem]">
-      <div className="flex justify-between ">
+      <div className="flex justify-between">
         <h3 className="flex flex-col justify-center text-white">
           Tip Amount{" "}
           <span className="text-[.8125rem] text-grayish-cyan">/ person</span>
         </h3>
-        <p className="pt-1 text-3xl text-strong-cyan lg:text-[2.9rem]">$4.27</p>
+        <p
+          className={twMerge(
+            "pt-1 text-strong-cyan",
+            tipAmount > 99999 ? "text-xl" : "text-3xl lg:text-[2.9rem]",
+          )}
+        >
+          ${tipAmount.toFixed(2)}
+        </p>
       </div>
       <div className="flex justify-between ">
         <h3 className="flex flex-col justify-center text-white">
           Total <span className="text-[13px] text-grayish-cyan">/ person</span>
         </h3>
-        <p className="pt-1 text-3xl text-strong-cyan lg:pt-0 lg:text-[2.9rem]">
-          $32.79
+        <p
+          className={twMerge(
+            "pt-1 text-strong-cyan lg:pt-0 ",
+            totalAmount > 99999 ? "text-xl" : "text-3xl lg:text-[2.9rem]",
+          )}
+        >
+          ${totalAmount.toFixed(2)}
         </p>
       </div>
     </div>
