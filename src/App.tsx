fa00e40a1ca2c5adf62f81f31607d24f6ac4e5aa
Aditya Phasu <adityaphasu919@gmail.com>
2024-05-28 20:44:17 +0530

    Update styling for page layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <main className="flex h-full flex-col gap-10 md:gap-7 xl:mt-[4.25rem] xl:gap-[5.5rem]">
       <h1 className="sr-only">Tip Calculator</h1>
-      <div className="flex justify-center pt-12 lg:pt-1">
+      <div className="flex justify-center pt-12 lg:pt-4">
         <img src={Logo} alt="" />
       </div>
       <div className="rounded-t-[1.55rem] bg-white md:rounded-3xl lg:mx-auto lg:max-w-[57.5rem]">
